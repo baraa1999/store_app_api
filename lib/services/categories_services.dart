e5d045b7bc9e14a3a35ca6f6a1e6c2f5a8d79de9
baraa1999 <baraaalian7@gmail.com>
2023-07-08 16:10:41 +0300

    get product by category

---
+++ lib/services/categories_services.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:store_app_api/models/product_model.dart';
+
+class CategoriesServices {
+  Future<List<ProductModel>> getCategoriesProducts(
+      {required String categoryName}) async {
+    http.Response response = await http.get(
+        Uri.parse('https://fakestoreapi.com/products/category/$categoryName'));
+    List<dynamic> data = jsonDecode(response.body);
+    List<ProductModel> productList = [];
+    for (int i = 0; i < data.length; i++) {
+      productList.add(
+        ProductModel.fromJson(data[i]),
+      );
+    }
+    return productList;
+  }
+}

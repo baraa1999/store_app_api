aa34df9d7624cb171cc8bce68d60c685b17fcc8f
baraa1999 <baraaalian7@gmail.com>
2023-07-08 15:41:47 +0300

    htttp in all products services

--- lib/services/get_all_product_services.dart
+++ lib/services/get_all_product_services.dart
@@ -1,1 +1,19 @@
-class GetAllProduct {}
+import 'dart:convert';
+
+import 'package:store_app_api/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class AllProductServices {
+  Future<List<ProductModel>> getAllProducts() async {
+    http.Response response =
+        await http.get(Uri.parse('https://fakestoreapi.com/products'));
+    List<dynamic> data = jsonDecode(response.body);
+    List<ProductModel> productList = [];
+    for (int i = 0; i < data.length; i++) {
+      productList.add(
+        ProductModel.fromJson(data[i]),
+      );
+    }
+    return productList;
+  }
+}

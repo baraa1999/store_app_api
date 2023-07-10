30b6ee0b2740054d15e0ba8bcfe1695c760516ab
baraa1999 <baraaalian7@gmail.com>
2023-07-10 15:51:33 +0300

    add product services

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -27,7 +27,12 @@ class Api {
       body: body,
       headers: headers,
     );
-    Map<String, dynamic> data = jsonDecode(response.body);
-    return data;
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'problems with status code ${response.statusCode} with body ${json.decode(response.body)} ');
+    }
   }
 }
--- lib/services/categories_services.dart
+++ lib/services/categories_services.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:store_app_api/helper/api.dart';
 
 import 'package:store_app_api/models/product_model.dart';

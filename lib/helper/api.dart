99d0dd0c362d12c7a9936f8d283fe5cff9d8704b
baraa1999 <baraaalian7@gmail.com>
2023-07-10 16:08:50 +0300

    put and update product servises

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -4,8 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<http.Response> get({required String url}) async {
-    http.Response response = await http.get(Uri.parse(url));
+  Future<http.Response> get(
+      {required String url, @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.get(Uri.parse(url), headers: headers);
 
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
@@ -19,6 +24,29 @@ class Api {
       @required dynamic body,
       @required String? token}) async {
     Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'problems with status code ${response.statusCode} with body ${json.decode(response.body)} ');
+    }
+  }
+
+  Future<dynamic> put(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
     if (token != null) {
       headers.addAll({'Authorization': 'Bearer $token'});
     }
---
+++ lib/services/update_product.dart
@@ -0,0 +1,23 @@
+import '../helper/api.dart';
+import '../models/product_model.dart';
+
+class UpdateProduct {
+  Future<ProductModel> updateProduct(
+      {required String title,
+      required String price,
+      required String description,
+      required String image,
+      required String category}) async {
+    Map<String, dynamic> data = await Api().post(
+      url: 'https://fakestoreapi.com/products',
+      body: {
+        'title': title,
+        'price': price,
+        'description': description,
+        'image': image,
+        'category': category
+      },
+    );
+    return ProductModel.fromJson(data);
+  }
+}

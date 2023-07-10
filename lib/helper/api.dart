e67a00366dff9dc93e6078a8c270479ad7216a7c
baraa1999 <baraaalian7@gmail.com>
2023-07-10 15:20:47 +0300

    post methode

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -11,5 +12,22 @@ class Api {
     } else {
       throw Exception('problem with status code ${response.statusCode}');
     }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+    Map<String, dynamic> data = jsonDecode(response.body);
+    return data;
   }
 }

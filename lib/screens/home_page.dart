fd9e550c7271b99b7c064b7987333da00fa32cc9
baraa1999 <baraaalian7@gmail.com>
2023-07-11 15:44:32 +0300

    stack and positioned widget in home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -26,50 +26,63 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Container(
-          height: 130,
-          width: 220,
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                  blurRadius: 40,
-                  color: Colors.grey.withOpacity(.2),
-                  spreadRadius: 0,
-                  offset: const Offset(8, 8)),
-            ],
-          ),
-          child: const Card(
-            elevation: 10,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Hand Bag Lv',
-                    style: TextStyle(color: Colors.grey, fontSize: 14),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        r'$225',
-                        style: TextStyle(color: Colors.grey, fontSize: 14),
-                      ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                      ),
-                    ],
-                  )
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: 130,
+              width: 220,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 40,
+                      color: Colors.grey.withOpacity(.2),
+                      spreadRadius: 0,
+                      offset: const Offset(8, 8)),
                 ],
               ),
+              child: const Card(
+                elevation: 10,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hand Bag Lv',
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            r'$225',
+                            style: TextStyle(color: Colors.grey, fontSize: 14),
+                          ),
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
+            Positioned(
+              right: 32,
+              top: -65,
+              child: Image.network(
+                'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+                height: 100,
+              ),
+            ),
+          ],
         ),
       ),
     );

716e62d116555805b5e40ee9e852df80c2c04a97
baraa1999 <baraaalian7@gmail.com>
2023-07-11 13:32:39 +0300

    home page ui heders

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  static String id = 'homePAge';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.cartPlus,
+                color: Colors.black,
+              ))
+        ],
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          'New Trend',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}

10004dbbcd3a2213cc4f194daa6cc9b87ccd7a68
baraa1999 <baraaalian7@gmail.com>
2023-07-11 15:12:25 +0300

    product card in home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -25,6 +25,53 @@ class HomePage extends StatelessWidget {
           style: TextStyle(color: Colors.black),
         ),
       ),
+      body: Center(
+        child: Container(
+          height: 130,
+          width: 220,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 40,
+                  color: Colors.grey.withOpacity(.2),
+                  spreadRadius: 0,
+                  offset: const Offset(8, 8)),
+            ],
+          ),
+          child: const Card(
+            elevation: 10,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hand Bag Lv',
+                    style: TextStyle(color: Colors.grey, fontSize: 14),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        r'$225',
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                      ),
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

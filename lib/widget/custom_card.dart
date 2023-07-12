0d67c37819dde3141374056f124553e41b21fdee
baraa1999 <baraaalian7@gmail.com>
2023-07-12 14:51:03 +0300

    grid view

---
+++ lib/widget/custom_card.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 50,
+                  color: Colors.grey.withOpacity(.2),
+                  spreadRadius: 0,
+                  offset: const Offset(8, 8)),
+            ],
+          ),
+          child: const Card(
+            elevation: 10,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hand Bag Lv',
+                    style: TextStyle(color: Colors.grey, fontSize: 14),
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        r'$225',
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                      ),
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 32,
+          top: -60,
+          child: Image.network(
+            'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+            height: 100,
+          ),
+        ),
+      ],
+    );
+  }
+}

566c67ff91529b97190f76b0408783fa1930f740
baraa1999 <baraaalian7@gmail.com>
2023-07-15 13:06:49 +0300

    update product body

--- lib/screens/update_product.dart
+++ lib/screens/update_product.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:store_app_api/widget/custom_button.dart';
+
+import '../widget/custom_text_field.dart';
 
 class UpdateProduct extends StatelessWidget {
-  const UpdateProduct({super.key});
+  UpdateProduct({super.key});
   static String id = 'update product';
+  String? productName, desc, img;
+  int? price;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +19,39 @@ class UpdateProduct extends StatelessWidget {
         ),
         backgroundColor: Colors.transparent,
         elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              CustomTextField(
+                onChanged: (data) {
+                  productName = data;
+                },
+                hintText: 'Product Name',
+              ),
+              const SizedBox(height: 10),
+              CustomTextField(
+                hintText: 'description',
+              ),
+              const SizedBox(height: 10),
+              CustomTextField(
+                hintText: 'Price',
+              ),
+              const SizedBox(height: 10),
+              CustomTextField(
+                hintText: 'image',
+              ),
+              const SizedBox(height: 50),
+              CustomButon(text: 'update'),
+            ],
+          ),
+        ),
       ),
     );
   }

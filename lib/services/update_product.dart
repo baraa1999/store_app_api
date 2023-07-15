12b2a524461789ad700e31f7ca080a56aa616e45
baraa1999 <baraaalian7@gmail.com>
2023-07-15 15:14:37 +0300

    oading indicator and handling exception

--- lib/services/update_product.dart
+++ lib/services/update_product.dart
@@ -8,7 +8,7 @@ class UpdateProductServices {
       required String description,
       required String image,
       required String category}) async {
-    Map<String, dynamic> data = await Api().post(
+    Map<String, dynamic> data = await Api().put(
       url: 'https://fakestoreapi.com/products',
       body: {
         'title': title,
